66404f9037368977f5618652e582574daed0a59a
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-07-29 21:04:23 -0400

    initial websockets server implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import qualified Data.Text as T
+import Control.Concurrent.Async
+import Control.Monad
+import Control.Concurrent.STM as STM
+import qualified Network.WebSockets as WS
 
 import Lib
 
+type Message = T.Text
+type MessageChannel = STM.TChan Message
+
+makeMessages :: IO MessageChannel
+makeMessages = atomically newTChan
+
+address = "127.0.0.1"
+port = 3000
+
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn $ "Starting server on " ++ address ++ ":" ++ show port
+  chan <- makeMessages
+
+  serv <- async $ WS.runServer address port (app chan)
+  wait serv
+
+app :: MessageChannel -> WS.ServerApp
+app chan pending = do
+  putStrLn "Accepting incoming connection"
+  conn <- WS.acceptRequest pending
+  handle chan conn
+
+handle :: MessageChannel -> WS.Connection -> IO ()
+handle channel conn = do
+  dupChannel <- atomically $ STM.dupTChan channel
+  concurrently_ (receive dupChannel) (send dupChannel)
+
+  where
+    receive chan = do
+      done <- async . forever $ do
+        message <- WS.receiveData conn
+        atomically $ STM.writeTChan chan message
+      wait done
+
+    send chan = do
+      done <- async . forever $ do
+        message <- atomically $ STM.readTChan chan
+        WS.sendTextData conn message
+      wait done
