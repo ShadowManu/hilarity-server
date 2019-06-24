b918b2efa5f92aa2628e7f6d7908c97fca3c92fc
Manuel Pacheco <manuelalejandropm@gmail.com>
2019-06-24 17:14:01 -0400

    cleanup messaging

--- app/Messaging.hs
+++ app/Messaging.hs
@@ -10,9 +10,11 @@ module Messaging
 , startBroadcast
 ) where
 
+import Prelude hiding ((!))
+
 import Control.Concurrent (forkIO, ThreadId)
-import qualified Control.Concurrent.Broadcast as Bc
 import Control.Monad (forever, forM_)
+import Data.Map ((!))
 import qualified Data.Map as M
 import qualified Network.WebSockets as WS
 import Control.Concurrent.STM
@@ -20,12 +22,11 @@ import qualified Data.Text as T
 import Control.Lens
 
 import qualified Types.Common as C
-import qualified Types.State as S
-import qualified Types.Game as G
 
 type Recipient = C.UserId
 type Response = T.Text
 
+-- Ways a message must be casted
 data Cast
   = Uni (Recipient, Response)
   | Multi [(Recipient, Response)]
@@ -34,8 +35,7 @@ data Cast
 
 data Messenger = Messenger
   { _cast :: TMVar Cast
-  , _recipients :: TVar (M.Map C.UserId WS.Connection)
-  }
+  , _recipients :: TVar (M.Map C.UserId WS.Connection) }
 
 makeLenses ''Messenger
 
@@ -43,34 +43,34 @@ new :: STM Messenger
 new = do
   recipients <- newTVar M.empty
   cast <- newEmptyTMVar
-  return $ Messenger { _recipients = recipients, _cast = cast }
+  return $ Messenger cast recipients
 
 add :: Recipient -> WS.Connection -> Messenger -> STM ()
 add recipient connection messenger =
-  modifyTVar (messenger ^. recipients) (at recipient ?~ connection)
+  modifyTVar
+    (messenger ^. recipients)
+    (at recipient ?~ connection)
 
 send :: Cast -> Messenger -> STM ()
 send c messenger = putTMVar (messenger ^. cast) c
 
 startBroadcast :: Messenger -> IO ThreadId
 startBroadcast messenger = forkIO . forever $ do
-  (cast, recipients) <- atomically $ do
-    cast <- takeTMVar . (^. cast) $ messenger
-    recipients <- readTVar . (^. recipients) $ messenger
+  (cast, rs) <- atomically $ do
+    cast <- takeTMVar $ messenger ^. cast
+    recipients <- readTVar $ messenger ^. recipients
     return (cast, recipients)
 
   case cast of
-    Uni (recipient, response) -> do
-      let connection = (M.!) recipients recipient
-      WS.sendTextData connection response
+    Uni (r, response) ->
+      WS.sendTextData (rs ! r) response
 
     Multi targets ->
-      forM_ targets $ \(recipient, response) ->
-        let connection = (M.!) recipients recipient
-        in WS.sendTextData connection response
+      forM_ targets $ \(r, response) ->
+        WS.sendTextData (rs ! r) response
 
     Broad response ->
-      forM_ (M.elems recipients) $ \connection ->
+      forM_ (M.elems rs) $ \connection ->
         WS.sendTextData connection response
 
     Raw connection response ->
