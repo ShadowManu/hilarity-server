632cee4dc4d4b7500f8bee041739374e89932a84
Manuel Pacheco <manuelalejandropm@gmail.com>
2021-06-12 18:03:16 -0400

    disable user existing verification

--- src/Hilarity/Server/Operations/Mods/User.hs
+++ src/Hilarity/Server/Operations/Mods/User.hs
@@ -13,18 +13,21 @@ import Hilarity.Server.Operations.Utils (assert)
 import qualified Hilarity.Server.Types.Card as Ca
 import Hilarity.Server.Types.Common (UserId)
 import qualified Hilarity.Server.Types.Deck as D
-import qualified Hilarity.Server.Types.Failure as F
-import qualified Hilarity.Server.Types.Game as G
-import qualified Hilarity.Server.Types.Hand as H
+import qualified Hilarity.Server.Types.Failure as Failure
+import qualified Hilarity.Server.Types.Game as Game
 import Hilarity.Server.Types.State (State)
-import qualified Hilarity.Server.Types.State as S
-import qualified Hilarity.Server.Types.Users as U
+import qualified Hilarity.Server.Types.State as State
+import qualified Hilarity.Server.Types.Users as Users
 import System.Random
 
 -- Add user to game
 addUser :: UserId -> Mod State UserId
 addUser name = do
-  exists <- use $ S.game . G.users . to (U.has name)
-  assert (not exists) (F.UserAlreadySignedIn name)
-  S.game . G.users %= U.add name
+  -- We won't check if the user exists, since the same user being added again
+  -- can be caused by a reconnection, and we will assume it as such.
+  -- In the future, the authentication system will have to help in the server
+  -- protecting other users being connected as other existing users
+  -- exists <- use $ State.game . Game.users . to (Users.has name)
+  -- assert (not exists) (Failure.UserAlreadySignedIn name)
+  State.game . Game.users %= Users.add name
   return name
